19e49fcf010b64497766ed979a354348aa4b4013
Maison Galvão <maisongalvao144@gmail.com>
2019-03-12 09:00:29 -0400

    Aula03 - Lançando Exceções

--- java-pilha/src/FluxoComTratamento.java
+++ java-pilha/src/FluxoComTratamento.java
@@ -1,5 +1,4 @@
-public class Fluxo {
-
+public class FluxoComTratamento {
     public static void main(String[] args) {
         System.out.println("Ini do main");
         try {
@@ -20,8 +19,8 @@ public class Fluxo {
 
     private static void metodo2() {
         System.out.println("Ini do metodo2");
-        ArithmeticException  exception = new ArithmeticException();
-        throw exception;
-       // System.out.println("Fim do metodo2");
+        throw new ArithmeticException("Deu erro");
+
+        // System.out.println("Fim do metodo2");
     }
 }
